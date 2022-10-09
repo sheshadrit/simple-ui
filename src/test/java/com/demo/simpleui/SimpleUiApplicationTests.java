a19dbecc4ae8dd3cee52d14d3cbede6c42cc1284
sheshadrit <sheshadritalla@gmail.com>
2022-10-09 14:50:15 -0400

    Initial project files

---
+++ src/test/java/com/demo/simpleui/SimpleUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.simpleui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
